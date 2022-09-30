ddbe02d5e8bb5bc585642613b0bd18284cde8a5f
Janne <t1laja03@students.oamk.fi>
2022-09-30 09:24:28 +0300

    Valmis esimerkki.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [age,setAge] = useState('')
+  const [hrl,setHRL] = useState('')
+
+  function calculate(e) {
+    e.preventDefault()
+    const Upper = (220-age) * 0.85
+    setHRL(Upper)
+    const Lower = (220-age) * 0.65
+    setHRL(Lower)
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="content">
+      <h3>Heart rate limits calculator</h3>
+      <form onSubmit={calculate}>
+        <div>
+          <label>Age</label>
+          <input value={age} onChange={e => setAge(e.target.value)}/>
+        </div>
+        <div>
+          <label>Heart rate limits</label>
+          <output>{hrl}</output>
+        </div>
+        <button>Calculate</button>
+      </form>
     </div>
   );
 }
